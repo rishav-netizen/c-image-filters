61c108b26450add7ca60d8ab5427c35958dfcafe
rishav-netizen <rishavkumar2k07@gmail.com>
2026-09-10 23:22:50 +0530

    tested reading function in main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "image.h"
 
 int main(int argc, char* argv[]) 
 {
+    Image* file = image_read("examples/test.bmp");
     
+    if (file==NULL)
+    {
+        printf("Could not read image file!\n");
+        free(file);
+        return 1;
+    }
+
+    printf("Yay!\n");
+    printf("%d %d \n", file->height, file->width);
     return 0;
 }
